acdf175571fe9834aba7fa8d74c275f029ac8584
Jasdev Singh <jasdev@live.in>
2014-03-07 23:56:16 -0800

    Added Practical 2

---
+++ P2 - Perceptron network for logical OR/perceptron-logical-OR.m
@@ -0,0 +1,28 @@
+% Script to make a Perceptron network for solving logical OR problem
+%
+% Soft Computing Lab (8th semester)
+%
+% Date: 24/2/2014
+%
+% Author: Jasdev Singh
+
+clc;
+clear all;
+close all;
+
+nnstart %open neural network GUI
+
+x = [0 1 0 1 0 1 0 1; 0 0 1 1 0 0 1 1; 0 0 0 0 1 1 1 1]; % input vector; 3-input AND gate
+
+t = [0 1 1 1 1 1 1 1]; % target vector
+
+net = perceptron; 
+
+net = train(net, x, t); % start training the network
+
+net.trainParam.epochs = 7; % 7 iterations
+
+view(net)
+
+y = net(x);
+
